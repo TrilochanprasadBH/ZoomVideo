7587e23205713629ad56607d850b23a633edcb00
TrilochanprasadBH <104064013+TrilochanprasadBH@users.noreply.github.com>
2023-10-28 21:17:01 +0530

    lobby user details

--- client/src/screens/LobbyScreen.jsx
+++ client/src/screens/LobbyScreen.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import  {useCallback, useState} from 'react'
 
 const LobbyScreen = () => {
+  const [email,setEmail]=useState("");
+  const [room, setRoom]=useState(""); 
+
+  const handleSubmit= useCallback((e)=>{
+   e.preventDefault()
+    console.log({email, room});
+  },[email, room]) 
+ 
+  //if dependecny is not given useCallback will run only on initial render [] , and upon changing input email room also , 
+  //console wont show updated values as dependency is empty 
+  
+  
+  
   return (
-    <div>LobbyScreen</div>
+    <div>
+      <h3>Video Lobby</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='email'>Email</label>
+        <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" id='email' />
+        <br />
+        <label htmlFor='room'>Room Number</label>
+        <input value={room} onChange={(e)=>setRoom(e.target.value)} type="number" id='room' />
+        <br />
+        <button>Join Room</button>
+      </form>
+    </div>
   )
 }
 
