bf211b2b3326131aa0bc3dc4be03cd50fe799a09
TrilochanprasadBH <104064013+TrilochanprasadBH@users.noreply.github.com>
2023-10-29 22:19:55 +0530

    socket emit on done

--- server/index.js
+++ server/index.js
@@ -5,4 +5,8 @@ const io= new Server(8080, {
 
 io.on("connection", (socket)=>{
     console.log("socket is connected, this is id",socket.id)
+    socket.on('room:join', (data)=>{
+        console.log(data);
+    })
 })
+
