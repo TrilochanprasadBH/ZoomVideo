16da61da8130d314ab0e4598b946605b2f600296
TrilochanprasadBH <104064013+TrilochanprasadBH@users.noreply.github.com>
2023-10-31 23:45:45 +0530

    listening fe

--- server/index.js
+++ server/index.js
@@ -3,10 +3,22 @@ const io= new Server(8080, {
     cors:true 
 })
 
+//to track which email is in which room 
+ const emailToSocket = new Map();
+ const SocketToEmail = new Map(); 
+
+
+
 io.on("connection", (socket)=>{
     console.log("socket is connected, this is id",socket.id)
+    //receiving from server 
     socket.on('room:join', (data)=>{
-        console.log(data);
+        // console.log(data);
+        const {email, room}= data 
+        emailToSocket.set(email, socket.id)
+        SocketToEmail.set(socket.id, email)
+       io.to(socket.id).emit('room:join', data);
+       //sending room join event from backend 
     })
 })
 
